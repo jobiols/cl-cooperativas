e1f14c9b626ac9c65a160f63999d4882d8e7ee55
Jorge Obiols <jobiols@users.noreply.github.com>
2020-04-01 14:28:04 -0300

    Update __manifest__.py

--- cooperativas_default/__manifest__.py
+++ cooperativas_default/__manifest__.py
@@ -76,7 +76,6 @@
         {'usr': 'oca', 'repo': 'web', 'branch': '9.0'},
         {'usr': 'oca', 'repo': 'server-tools', 'branch': '9.0'},
         {'usr': 'oca', 'repo': 'social', 'branch': '9.0'},
-        {'usr': 'oca', 'repo': 'server-brand', 'branch': '9.0'},
         {'usr': 'oca', 'repo': 'manufacture', 'branch': '9.0'},
         {'usr': 'oca', 'repo': 'manufacture-reporting', 'branch': '9.0'},
         {'usr': 'oca', 'repo': 'management-system', 'branch': '9.0'},
